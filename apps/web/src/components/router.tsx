d260f937ee416a0314dd11072f229029cdc07294
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-04-07 18:04:25 -0300

    fix: add remaining routes and layouts

--- apps/web/src/components/router.tsx
+++ apps/web/src/components/router.tsx
@@ -1,14 +1,17 @@
-import { Home } from "lucide-react";
 import { Route, Routes } from "react-router";
 
+// Layouts
 import { AuthLayout } from "@/layouts/auth-layout";
+
+// Pages
+import { HomePage } from "@/pages/home";
 import { LoginPage } from "@/pages/login";
 import { RegisterPage } from "@/pages/register";
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route index element={<Home />} />
+      <Route index element={<HomePage />} />
       <Route path="auth">
         <Route element={<AuthLayout />}>
           <Route path="login" element={<LoginPage />} />
