ea4d3108309c2dfc47d27ea27530749a2b50d47e
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-04-08 18:48:19 -0300

    feat: add dinamically header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,6 +1,9 @@
+import { useAuth } from "@/modules/auth/hooks/use-auth";
 import { Link } from "react-router";
 
 export function Header() {
+  const { isAuthenticated } = useAuth();
+
   return (
     <header className="py-5 px-3 lg:px-0 border-b border-border">
       <div className="max-w-5xl mx-auto flex justify-between">
@@ -8,18 +11,31 @@ export function Header() {
           Task Manager
         </Link>
         <div className="space-x-3">
-          <Link
-            to="/auth/login"
-            className="bg-purple-500 px-6 py-2 rounded-md text-white font-semibold"
-          >
-            Login
-          </Link>
-          <Link
-            to="/auth/register"
-            className="hover:bg-[#dde3eb] transition-colors px-6 py-2 rounded-md duration-200 font-semibold"
-          >
-            Register
-          </Link>
+          {isAuthenticated ? (
+            <>
+              <Link
+                to="/dashboard"
+                className="bg-purple-500 px-6 py-2 rounded-md text-white font-semibold"
+              >
+                Go to dashboard
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                to="/auth/login"
+                className="bg-purple-500 px-6 py-2 rounded-md text-white font-semibold"
+              >
+                Login
+              </Link>
+              <Link
+                to="/auth/register"
+                className="hover:bg-[#dde3eb] transition-colors px-6 py-2 rounded-md duration-200 font-semibold"
+              >
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
